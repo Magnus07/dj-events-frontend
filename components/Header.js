65e0251a9222ba8a18a7ab138e5750e213469edd
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-04-25 12:56:48 +0300

    Login & Register pages

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Search from "./Search";
 import styles from "../styles/Header.module.css";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 function Header() {
   return (
     <header className={styles.header}>
@@ -21,6 +22,13 @@ function Header() {
           <li>
             <Link href="/events/add" legacyBehavior>
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login" legacyBehavior>
+              <a className="btn-secondary btn-icon">
+                <FaSignInAlt /> Login
+              </a>
             </Link>
           </li>
         </ul>

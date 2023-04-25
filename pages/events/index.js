34b53c34d0df28e6b831d2d3ba839fa479f89db4
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-04-25 12:27:17 +0300

    Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,9 @@
 import Layout from "@/components/Layout";
-import { API_URL } from "@/config";
+import { API_URL, PER_PAGE } from "@/config";
 import EventItem from "@/components/EventItem";
+import Pagination from "@/components/Pagination";
 
-export default function Home({ events }) {
+export default function Home({ events, page, total }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
@@ -11,17 +12,24 @@ export default function Home({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC&populate=*`);
-  var events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&pagination[limit]=${PER_PAGE}&pagination[start]=${start}&populate=*`
+  );
+  var events = await eventRes.json();
+  var total = events.meta.pagination.total;
+
   events = events.data;
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
